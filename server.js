6550214d58dd68c8aac687e4513af0d17838e6df
Luke Wendling <luke@lukewendling.com>
2016-02-02 15:38:52 -0600

    pass tiles url in demo. node will serve static tiles cache if given env var

--- server.js
+++ server.js
@@ -1,19 +1,30 @@
+require('dotenv').load({silent: true});
 var compression = require('compression');
 var express = require('express');
 var serveStatic = require('serve-static');
 var app = express();
+var tilesPath = process.env.TILES_PATH;
+var port = process.env.PORT || 3000;
 
 app.use(compression());
 
+// serve Angular app
 app.use(serveStatic('client'));
-app.use(serveStatic('test'));
+
+// optionally serve cached tiles from filesystem
+if (tilesPath) {
+  app.use('/tiles', serveStatic(tilesPath));
+}
 
 app.set('views', './client/app/templates')
 app.set('view engine', 'jade');
 
+// render Angular templates
 app.get('/templates/:template', (req, res) => {
   var template = req.params.template;
   res.render(template, {});
 });
 
-app.listen(process.env.PORT || 3000);
+app.listen(port, () => {
+  console.info('Listening on port %s', port);
+});
